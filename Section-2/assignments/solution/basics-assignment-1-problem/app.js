a38a298ee39cc78ebeaf2545f30caf6c13cd9946
Jagriti Khanna <jkhanna@goodmorning.com>
2024-02-08 10:26:25 -0700

    changed variables for assignment #1

--- Section-2/assignments/solution/basics-assignment-1-problem/app.js
+++ Section-2/assignments/solution/basics-assignment-1-problem/app.js
@@ -1,17 +1,22 @@
 const app = Vue.createApp({
     data() {
         return {
-            userName: 'Jagriti Khanna',
-            userAge: '22',
-            onlineImage: 'https://www.juno.ca/wp-content/uploads/Juno-HeroContent-20221108-LR-15.jpg'
+            name: 'Jagriti Khanna',
+            age: '22',
+            imageURL: 'https://www.juno.ca/wp-content/uploads/Juno-HeroContent-20221108-LR-15.jpg'
         };
     },
     methods: { 
-        futureAge() {
-            const futureAge = Number(this.userAge) + 5; //The Number(this.userAge) part converts the userAge variable to a number, ensuring that the addition operation is performed correctly.
+        // futureAge() {
+        //     const futureAge = Number(this.userAge) + 5; //The Number(this.userAge) part converts the userAge variable to a number, ensuring that the addition operation is performed correctly.
+        //     return futureAge;
+        // },
+        // OR
+        calculateAge() {
+            const futureAge = this.age + 5;
             return futureAge;
         },
-        randomNumber() {
+        favNumber() {
             const randomNumber = Math.random();
             return randomNumber;
         }
